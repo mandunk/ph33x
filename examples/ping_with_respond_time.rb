fda547f3d139b87b1231088b2a1ec4d30b9f1c34
mandunk <manwatchdog@gmail.com>
2016-08-20 21:59:48 -0400

    Update ping_with_respond_time.rb

--- examples/ping_with_respond_time.rb
+++ examples/ping_with_respond_time.rb
@@ -4,8 +4,8 @@ require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new token: 'B0T.T0KEN.here', application_id: 160123456789876543, prefix: '!'
 
-bot.command(:ping) do |event|
-  m = event.respond('Pong!')
+bot.command(:pheix) do |event|
+  m = event.respond('DO YOU WISH TO ENGAGE IN COMBAT')
   m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
   nil
 end
